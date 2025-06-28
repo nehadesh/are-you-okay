1f1371fa48babe6fb22bf829c420d995917d9f54
Nimita Deshpande <nimita.ahs@gmail.com>
2025-06-28 19:25:07 -0400

    Edited dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
+import News from "./News";
 
 export default function SetupPage() {
-
   return (
     <>
     <div className="bg-gray-50 py-24 sm:py-32">
@@ -28,6 +28,7 @@ export default function SetupPage() {
           <div className="px-8 pt-8 sm:px-10 sm:pt-10">
             <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
             <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.</p>
+            <News location="San Francisco" />
           </div>
           <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
             <img className="w-full max-lg:max-w-xs" src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-performance.png" alt="" />
